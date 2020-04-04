988d2f624795ee08e7e30c7712eac1d2fe881b6d
Sai Rohith Reddy Vangala <sairohit.vangala@vannainfotech.com>
2020-04-05 00:14:31 +0530

    Created sample for tuples

---
+++ TuplesApplication/TuplesApplication/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace TuplesApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentManagement management = new StudentManagement();
+            var details = ReadDetails();
+            management.Age = details.age;
+            management.Name = details.name;
+            management.SGPA = details.SGPA;
+            management.Grade = details.grade;
+            PrintDetails(management);
+        }
+
+        static (string name, IList<Double> SGPA, int age, int grade) ReadDetails()
+        {
+            IList<Double> SGPA = new List<Double>();
+            Console.WriteLine("Enter the student's name");
+            string studentName = Console.ReadLine();
+            Console.WriteLine("Enter the student's age");
+            if (!int.TryParse(Console.ReadLine(), out int studentAge))
+            {
+                Console.WriteLine("Age cannot be anything but an integer.");
+            }
+            Console.WriteLine("Enter the student's grade.");
+            if (!int.TryParse(Console.ReadLine(), out int studentGrade))
+            {
+                Console.WriteLine("Grade must be an integer!");
+            }
+            Console.WriteLine("Enter the student's SGPA for every semester. Enter 0 if you've finished.");
+            while (true)
+            {
+                double sgpa = Convert.ToDouble(Console.ReadLine(), new CultureInfo("en-US").NumberFormat);
+                if (sgpa != 0)
+                {
+                    SGPA.Add(sgpa);
+                }
+                else
+                    break;
+            }
+            return (studentName, SGPA, studentAge, studentGrade);
+        }
+
+        static void PrintDetails(StudentManagement studentManagement)
+        {
+            (string name, double gpa) = studentManagement.CalculateCGPA();
+            var studentGeneralData = studentManagement.GetStudentGeneralInformation();
+            Console.WriteLine(name + " of grade " + studentGeneralData.studentGrade + " has scored " + gpa);
+        }
+    }
+}
---
+++ TuplesApplication/TuplesApplication/StudentManagement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TuplesApplication
+{
+    class StudentManagement
+    {
+        public string Name { private get; set; }
+        public int Grade { private get; set; }
+        public int Age { private get; set; }
+        public IList<Double> SGPA { private get; set; }
+
+        public (string studentName, int studentGrade) GetNameAndGrade()
+        {
+            var studentInfo = (this.Name, grade: this.Grade);
+            return studentInfo;
+        }
+
+        public (string studentName, int studentGrade, int studentAge) GetStudentGeneralInformation()
+        {
+            var studentGeneralInfo = (this.Name, this.Grade, this.Age);
+            return studentGeneralInfo;
+        }
+
+        public (string studentName, double CGPA) CalculateCGPA()
+        {
+            var academicStats = (name: this.Name, CGPA: 0.0);
+            foreach (var gpa in this.SGPA)
+            {
+                academicStats.CGPA += gpa;
+            }
+            academicStats.CGPA /= this.SGPA.Count;
+            return academicStats;
+        }
+    }
+}
