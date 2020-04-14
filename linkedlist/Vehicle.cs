df34ebf0beee6400ade85adfce1a3fc80dfa70db
Sai Rohith Reddy Vangala <sairohit.vangala@vannainfotech.com>
2020-04-14 15:05:51 +0530

    Add basic code sample for linkedlist

---
+++ linkedlist/Vehicle.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace linkedlist
+{
+    public class Vehicle
+    {
+        public String brandName {get; set;}
+        public String engineSerial {get; set;}
+        public int wheelCount {get; set;}
+        public bool isRegistered {get; set;}
+    }
+}
